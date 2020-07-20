c116121074c8ea82f4b424d86bcc04666ff5371a
Chris Aquino <chris@radishmouse.com>
2020-07-20 15:02:05 -0400

    updates setup-psql.sh to work in WSL

--- setup-psql.sh
+++ setup-psql.sh
@@ -1,8 +1,18 @@
+#!/bin/bash
+
+echo "Installing postgresql"
 sudo apt update && sudo apt install -y postgresql
 
-sudo su - postgres -c "psql -c 'create role ubuntu; alter user ubuntu with createdb superuser login;'"
+USER=$(whoami)
+PSQL_VERSION=$(psql --version | awk '{print $3}' | awk -F. '{print $1}')
+PSQL_CONFIG="/etc/postgresql/$PSQL_VERSION/main/pg_hba.conf"
 
-sudo sed -i '/^host/ s/md5/trust/' /etc/postgresql/10/main/pg_hba.conf
+echo "Creating user $USER"
+sudo su - postgres -c "psql -c 'create role $USER; alter user $USER with createdb superuser login;'"
+echo "Updating configuration file $PSQL_CONFIG"
+sudo sed -i '/^host/ s/md5/trust/' "$PSQL_CONFIG"
 
+echo "Restarting postgresql"
 sudo service postgresql restart
 
+echo "Finished!"
